00320fcc530bbadc3417e546cdc23ceacf34833f
jdrichards-pursuit <jdrpursuit@gmail.com>
2023-11-09 15:28:38 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Emojihub DOM Manipulation
+
+### Instructions
+
+- `Clone` this repo.
+
+- Using the `data` array and JS DOM Manipulation only, place each image in a `span` tag and add all of them in the `main` element in the DOM.
+
+- Do not change the HTML or CSS files.
---
+++ main.js
@@ -0,0 +1,75 @@
+const data = [
+  {
+    name: "grinning face",
+    category: "smileys and people",
+    group: "face positive",
+    htmlCode: ["&#128512;"],
+    unicode: ["U+1F600"],
+  },
+  {
+    name: "grinning face with smiling eyes",
+    category: "smileys and people",
+    group: "face positive",
+    htmlCode: ["&#128513;"],
+    unicode: ["U+1F601"],
+  },
+  {
+    name: "face with tears of joy",
+    category: "smileys and people",
+    group: "face positive",
+    htmlCode: ["&#128514;"],
+    unicode: ["U+1F602"],
+  },
+  {
+    name: "rolling on the floor laughing",
+    category: "smileys and people",
+    group: "face positive",
+    htmlCode: ["&#129315;"],
+    unicode: ["U+1F923"],
+  },
+  {
+    name: "face with stuck-out tongue and tightly-closed eyes ≊ face with stuck-out tongue ",
+    category: "smileys and people",
+    group: "face neutral",
+    htmlCode: ["&#128541;"],
+    unicode: ["U+1F61D"],
+  },
+  {
+    name: "drooling face",
+    category: "smileys and people",
+    group: "face neutral",
+    htmlCode: ["&#129316;"],
+    unicode: ["U+1F924"],
+  },
+  {
+    name: "pensive face",
+    category: "smileys and people",
+    group: "face neutral",
+    htmlCode: ["&#128532;"],
+    unicode: ["U+1F614"],
+  },
+  {
+    name: "upside-down face",
+    category: "smileys and people",
+    group: "face neutral",
+    htmlCode: ["&#128579;"],
+    unicode: ["U+1F643"],
+  },
+  {
+    name: "money-mouth face",
+    category: "smileys and people",
+    group: "face neutral",
+    htmlCode: ["&#129297;"],
+    unicode: ["U+1F911"],
+  },
+];
+
+function run() {
+  const main = document.querySelector("main");
+
+  data.forEach(
+    ({ htmlCode }) => (main.innerHTML += `<span>${htmlCode[0]}</span>`)
+  );
+}
+
+run();
